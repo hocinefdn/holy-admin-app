57dc45fadcb72fa45e21d224c5f397da73357d93
hocinefdn <hocinefedani@gmail.com>
2023-02-27 18:49:24 +0100

    :lipstick: Update Admin home page

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -7,39 +7,37 @@ const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Paper elevation={3}>
-          <Toolbar>
+        <Toolbar>
+          <div className="flex flex-row items-center absolute left-10">
             <Link to="/">
               <img src={logo} alt="logo" width="60" />
             </Link>
+            <Link to="/tariffs" className="text-lg mr-2 hover:underline ">
+              Tarifs
+            </Link>
+            <Link to="/contact" className="text-lg mr-2 hover:underline ">
+              Contact
+            </Link>
+          </div>
+
+          <div className="flex justify-between absolute right-4 items-center">
             <div className="flex justify-end">
-              <Link to="/tariffs" className="mr-2 hover:underline">
-                Tarifs
+              <Link to="/login" className="text-lg mr-2 hover:underline ">
+                Se connecter
               </Link>
-              <Link to="/contact" className="mr-2 hover:underline">
-                Contact
+              <Link to="/register" className="text-lg mr-2 hover:underline ">
+                S'inscrire
               </Link>
             </div>
 
-            <div className="flex justify-between absolute right-4 items-center">
-              <div className="flex justify-end">
-                <Link to="/login" className="mr-2 hover:underline">
-                  Se connecter
-                </Link>
-                <Link to="/register" className="mr-2 hover:underline">
-                  S'inscrire
-                </Link>
-              </div>
-
-              <Link
-                to="/test"
-                className="cursor-pointer bg-red-500 px-3 py-2 font-bold text-md rounded-3xl hover:bg-red-700 text-white"
-              >
-                Démarrer l’essai gratuit
-              </Link>
-            </div>
-          </Toolbar>
-        </Paper>
+            <Link
+              to="/test"
+              className="cursor-pointer bg-red-500 px-3 py-2 font-bold text-md rounded-3xl hover:bg-red-700 text-white"
+            >
+              Démarrer l’essai gratuit
+            </Link>
+          </div>
+        </Toolbar>
       </AppBar>
     </Box>
   );
